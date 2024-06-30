75fc23697a966e70e79b05fd65d2cf60832d22bd
Jamin Chan <jaminchan321@gmail.com>
2024-06-29 23:58:44 -0700

    added market data

--- bot.py
+++ bot.py
@@ -1,6 +1,9 @@
 
-import requests
 import alpaca_trade_api as tradeapi
+import pandas as pd
+import numpy as np
+import time, requests, datetime, json
+
 
 from config import API_KEY
 from config import SECRET_KEY
@@ -20,6 +23,11 @@ except:
     print("URL error")
 
 
+symbol = 'BTCUSD'
+dataSymbol = 'BTC/USD'
+short_ma_length = 50
+long_ma_length = 200
+percent_of_equity = 0.25
 
 def getAccountInfo():
     try:
@@ -63,6 +71,8 @@ def buy_stock(symbol, qty):
         print(f"API Error: {e}")
         return None
 
+
+
 def sell_stock(symbol, qty):
     try:
         # Place a market order to sell the specified quantity of the stock
@@ -78,6 +88,35 @@ def sell_stock(symbol, qty):
     except tradeapi.rest.APIError as e:
         print(f"API Error: {e}")
         return None
-    
 
-buy_stock("AAPL", 100)
+
+
+def getMarketData(symbol):
+    now = datetime.datetime.now(datetime.UTC)
+    print(now)
+    startTime = now - datetime.timedelta(hours=5)
+    converted_datetime_str = startTime.strftime('%Y-%m-%dT%H:%M:%SZ')
+    print(converted_datetime_str)
+
+    #url = "https://data.alpaca.markets/v1beta3/crypto/us/bars?symbols={symbol}&start={}&end={now}&limit=1000&sort=asc"
+    #url = f'https://data.alpaca.markets/v1beta3/crypto/us/latest/bars?symbols={symbol}'
+    url = f'https://data.alpaca.markets/v1beta3/crypto/us/bars?symbols={symbol}&start={converted_datetime_str}&timeframe=5Min&limit=1000&sort=asc'
+
+    headers = {
+        "accept": "application/json",
+        #"APCA-API-KEY-ID": API_KEY,
+        #"APCA-API-SECRET-KEY": SECRET_KEY
+    }
+    response = requests.get(url, headers=headers)
+    marketData = json.loads(response.text)
+    # c = closing, h = high, l = low, n = no idea, o = opening, t = time, v = volume, vw = volume weighted/vwap
+    for i in marketData["bars"][symbol]:
+        print(i)
+    # goal, get ema and make changes based on ema
+    return None
+
+
+#buy_stock(symbol, 1)
+#sell_stock(symbol, 1)
+getMarketData(dataSymbol)
+#getAccountInfo()
